03d7c2bbe383ebd2dd214ccf917968b57f77b583
jseiter <joes@webappsolution.com>
2020-11-13 11:08:42 -0500

    Use epics

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
-// import {createEpicMiddleware} from 'redux-observable';
-import createSagaMiddleware from 'redux-saga'
-// import epics from './epics';
-import sagas from './sagas';
+import {createEpicMiddleware} from 'redux-observable';
+// import createSagaMiddleware from 'redux-saga'
+import epics from './epics';
+// import sagas from './sagas';
 // import thunk from 'redux-thunk';
 import {logger} from 'redux-logger';
 import reducer from './reducers';
@@ -27,22 +27,23 @@ const initialState = {
 //     applyMiddleware(thunk, logger)
 // );
 
-// const epicMiddleware = createEpicMiddleware();
-// const store = createStore(
-//     reducer,
-//     initialState,
-//     applyMiddleware(epicMiddleware, logger)
-// );
-
-const sagaMiddleware = createSagaMiddleware();
-
+const epicMiddleware = createEpicMiddleware();
 const store = createStore(
     reducer,
     initialState,
-    applyMiddleware(sagaMiddleware, logger)
+    applyMiddleware(epicMiddleware, logger)
 );
 
-sagaMiddleware.run(sagas);
+epicMiddleware.run(epics);
+
+// const sagaMiddleware = createSagaMiddleware();
+// const store = createStore(
+//     reducer,
+//     initialState,
+//     applyMiddleware(sagaMiddleware, logger)
+// );
+//
+// sagaMiddleware.run(sagas);
 
 ReactDOM.render(
     <Provider store={store} >
